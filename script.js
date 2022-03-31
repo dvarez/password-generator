3037d4cfea12b9b778f1319b7632f1817ac48a92
Dietrich Varez <dietrich1899@gmail.com>
2022-03-31 05:02:05 -0700

    read

--- script.js
+++ script.js
@@ -71,9 +71,6 @@ function generatePassword(){
 
 
   return pass
-
-
-
 }
 
 
@@ -86,7 +83,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
